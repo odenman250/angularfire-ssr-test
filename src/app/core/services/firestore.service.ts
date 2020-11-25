aaae00fd9254d44eab4559193eeff14b2c1deb0a
odenman250 <odenman250@gmail.com>
2020-11-25 16:51:22 +1000

    Update to firebase@8.0.1

--- src/app/core/services/firestore.service.ts
+++ src/app/core/services/firestore.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { startWith, tap } from 'rxjs/operators';
-import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { TransferState } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class FirestoreService {
+	constructor(private state: TransferState, private firestore: AngularFirestore) {}
 
-  constructor(private state: TransferState, private firestore: AngularFirestore) {}
-
-  getCollection<T>(collectionRef: string): Observable<T[]> {
-    const collection = this.firestore.collection<T>(collectionRef, ref => ref.where('active', '==', true));
-    return collection.valueChanges();
-  }
-
+	getCollection<T>(collectionRef: string): Observable<T[]> {
+		const collection = this.firestore.collection<T>(collectionRef, ref => ref.where('active', '==', true));
+		return collection.valueChanges();
+	}
 }
